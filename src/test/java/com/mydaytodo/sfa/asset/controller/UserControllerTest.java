4d5a8250de25522f3e21843e65face0540e3ccb9
Bhuman Soni <bhuman@mydaytodo.com>
2023-10-19 08:41:01 +1100

    update user method now works and updated the controller unit test

--- src/test/java/com/mydaytodo/sfa/asset/controller/UserControllerTest.java
+++ src/test/java/com/mydaytodo/sfa/asset/controller/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.mydaytodo.sfa.asset.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mydaytodo.sfa.asset.model.AssetUser;
+import com.mydaytodo.sfa.asset.model.CreateUserRequest;
+import com.mydaytodo.sfa.asset.model.ServiceResponse;
 import com.mydaytodo.sfa.asset.service.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,16 +13,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -33,14 +39,14 @@ public class UserControllerTest {
     private final String BASE_URL = "/api/user";
     JacksonTester<AssetUser> userJacksonTester;
     private final String MOCK_USER_ID = "USR_123";
-    private AssetUser user;
+    private AssetUser assetUser;
     private MockMvc mockMvc;
 
     @BeforeEach
     void populate() {
-        user = AssetUser.builder()
+        assetUser = AssetUser.builder()
                 .name("")
-                // .assetsUploaded(new String[]{"file1.pdf", "file2.docx"})
+                .assetsUploaded(Arrays.asList("file1.pdf", "file2.docx"))
                 .role("owner")
                 .department("finance")
                 .dateJoined(Date.from(Instant.now()))
@@ -57,33 +63,10 @@ public class UserControllerTest {
     }
     @Test
     void testGetUser() throws Exception {
-        /*when(userService.getUser(any())).thenReturn(user);
-        mockMvc.perform(get(BASE_URL + "/")
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isOk());*/
+        when(userService.getUser(any())).thenReturn(ServiceResponse.builder().data(assetUser).status(200).build());
+        String url = BASE_URL + "/"+MOCK_USER_ID;
+        mockMvc.perform(get(url)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
-    @Test
-    void testCreateUser() throws Exception  {
-        // when(userService.createUser(any())).thenReturn(HttpStatus.CREATED.value());
-        mockMvc.perform(post(BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(userJacksonTester.write(user).getJson())
-        ).andExpect(status().isOk());
-    }
-    @Test
-    void testUpdateUser() throws Exception {
-        // when(userService.updateUser(any(), any()).thenReturn(HttpStatus.NO_CONTENT.value()));
-        mockMvc.perform(put(BASE_URL + "/" + MOCK_USER_ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(userJacksonTester.write(user).getJson())
-        ).andExpect(status().isNoContent());
-
-    }
-    @Test
-    void testDeleteUser() throws Exception {
-        // when(userService.deleteUser(any()).thenReturn(HttpStatus.NO_CONTENT.value()));
-        mockMvc.perform(delete(BASE_URL + "/" + MOCK_USER_ID)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
-    }*/
 }
